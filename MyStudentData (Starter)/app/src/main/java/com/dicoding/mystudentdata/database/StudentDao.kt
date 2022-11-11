3b71f16a3ece61737231a1f8ca93d6ff12ea211c
aliefabdillah <aliefmabdillah09@gmail.com>
2022-11-11 10:18:56 +0700

    Commit - membuat app relasi database, single, one to many, many to one, dan many to many

---
+++ MyStudentData (Starter)/app/src/main/java/com/dicoding/mystudentdata/database/StudentDao.kt
@@ -0,0 +1,36 @@
+package com.dicoding.mystudentdata.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface StudentDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertStudent(student: List<Student>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertUniversity(university: List<University>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertCourse(course: List<Course>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertCourseStudentCrossRef(courseStudentCrossRef: List<CourseStudentCrossRef>)
+
+    @Query("SELECT * from student")
+    fun getAllStudent(): LiveData<List<Student>>
+
+    //transaction digunakan untuk menjalankan lebih dari satu query secara bersama
+    @Transaction
+    //memanggil kelas utama dan kembalian berupa relasi
+    @Query("SELECT * from student")
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>>
+
+    @Transaction
+    @Query("SELECT * from university")
+    fun getAllUnivAndStudent(): LiveData<List<UniversityAndStudent>>
+
+    @Transaction
+    @Query("SELECT * from student")
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>>
+}
