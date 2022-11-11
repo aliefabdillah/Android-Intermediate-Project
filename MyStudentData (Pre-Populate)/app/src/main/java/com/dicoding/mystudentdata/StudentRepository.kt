134699af56213efc4be82ae529820cea0fe829c3
aliefabdillah <aliefmabdillah09@gmail.com>
2022-11-11 10:55:14 +0700

    Commit - Membuat app dapat menggunakan database ketika pertama kali di install dengan addCallback pada saat build database

---
+++ MyStudentData (Pre-Populate)/app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
@@ -0,0 +1,23 @@
+package com.dicoding.mystudentdata
+
+import androidx.lifecycle.LiveData
+import com.dicoding.mystudentdata.database.*
+import com.dicoding.mystudentdata.helper.InitialDataSource
+
+class StudentRepository(private val studentDao: StudentDao) {
+    fun getAllStudent(): LiveData<List<Student>> = studentDao.getAllStudent()
+
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentDao.getAllStudentAndUniversity()
+
+    fun getAllUnivAndStudent(): LiveData<List<UniversityAndStudent>> = studentDao.getAllUnivAndStudent()
+
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> = studentDao.getAllStudentWithCourse()
+
+//    suspend fun insertAllData() {
+//        studentDao.insertStudent(InitialDataSource.getStudents())
+//        studentDao.insertUniversity(InitialDataSource.getUniversities())
+//        studentDao.insertCourse(InitialDataSource.getCourses())
+//        studentDao.insertCourseStudentCrossRef(InitialDataSource.getCourseStudentRelation())
+//    }
+
+}
